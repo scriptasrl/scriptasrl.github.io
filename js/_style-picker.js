6ab78a615d732c3c09ee6de50f067e55a11290c4
Yanez Diego Parolin <yanezprl@gmail.com>
2020-05-01 12:56:14 +0200

    Update _style-picker.js

--- js/_style-picker.js
+++ js/_style-picker.js
@@ -11,4 +11,7 @@
 	});
 })();
 
+
+var colors = ["#2ecc71","#3498db","#9b59b6", "#f1c40f", "#e67e22", "#e74c3c"];                
+var rand = Math.floor(Math.random()*colors.length);
 $('header').css({"background":"red"});
